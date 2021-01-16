09cd6ec3bad2d5fefeb07588381c73820a7a89a9
Mirko Rapisarda <mirkorap16@gmail.com>
2021-01-16 15:21:51 +0100

    chore: install and configure firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:taberu/injection.dart';
 import 'package:taberu/locale.dart';
 import 'package:taberu/themes/app_theme.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   configureInjection(Environment.prod);
+  await Firebase.initializeApp();
   runApp(const AppLocale(child: TaberuApp()));
 }
 
