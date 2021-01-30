6b3704a41b6e843a44dbfff60367b5385d1d0acb
Mirko Rapisarda <mirkorap16@gmail.com>
2021-01-30 15:26:36 +0100

    refactor: move SplashScreen to restaurant_menu module

--- lib/router.dart
+++ lib/router.dart
@@ -1,5 +1,5 @@
 import 'package:auto_route/auto_route_annotations.dart';
-import 'package:taberu/core/presentation/splash/splash_screen.dart';
+import 'package:taberu/restaurant_menu/presentation/splash/splash_screen.dart';
 import 'package:taberu/restaurant_menu/presentation/restaurant_selection/restaurant_selection_screen.dart';
 
 @MaterialAutoRouter(routes: <AutoRoute>[
