6546517edbdff400c3168299abfe0e2cb76c9bf9
Mirko Rapisarda <mirkorap16@gmail.com>
2021-02-07 11:52:10 +0100

    improvement: add default label text

--- lib/themes/app_input_decoration.dart
+++ lib/themes/app_input_decoration.dart
@@ -1,13 +1,15 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:stilo/stilo.dart';
 
 abstract class AppInputDecoration {
-  static const searchTextField = InputDecoration(
+  static final searchTextField = InputDecoration(
     filled: true,
     floatingLabelBehavior: FloatingLabelBehavior.never,
     contentPadding: StiloEdge.a2,
-    prefixIcon: Icon(Icons.search),
-    border: OutlineInputBorder(
+    prefixIcon: const Icon(Icons.search),
+    labelText: tr('app.actions.search'),
+    border: const OutlineInputBorder(
       borderSide: BorderSide.none,
       borderRadius: BorderRadius.all(
         Radius.circular(100),
