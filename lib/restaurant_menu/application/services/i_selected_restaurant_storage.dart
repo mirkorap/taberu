ea8016566d537bec9cc35e73356be2b84da9f4b5
Mirko Rapisarda <mirkorap16@gmail.com>
2021-02-14 15:32:22 +0100

    refactor: remove interface to get/set selected restaurant

--- lib/restaurant_menu/application/services/i_selected_restaurant_storage.dart
+++
@@ -1,7 +0,0 @@
-import 'package:taberu/restaurant_menu/domain/entities/restaurant.dart';
-
-abstract class ISelectedRestaurantStorage {
-  Restaurant getRestaurant();
-
-  Future<bool> setRestaurant(Restaurant restaurant);
-}
--- lib/restaurant_menu/infrastructure/services/selected_restaurant_storage.dart
+++ lib/restaurant_menu/infrastructure/services/selected_restaurant_storage.dart
@@ -1,20 +1,16 @@
 import 'dart:convert';
 
-import 'package:injectable/injectable.dart';
 import 'package:taberu/core/application/services/i_device_storage.dart';
-import 'package:taberu/restaurant_menu/application/services/i_selected_restaurant_storage.dart';
 import 'package:taberu/restaurant_menu/domain/entities/restaurant.dart';
 import 'package:taberu/restaurant_menu/infrastructure/data_transfer_objects/restaurant_dto.dart';
 
-@LazySingleton(as: ISelectedRestaurantStorage)
-class SelectedRestaurantStorage implements ISelectedRestaurantStorage {
+class SelectedRestaurantStorage {
   static const storageKey = 'selected_restaurant';
 
   final IDeviceStorage _deviceStorage;
 
   SelectedRestaurantStorage(this._deviceStorage);
 
-  @override
   Restaurant getRestaurant() {
     final jsonRestaurant = jsonDecode(
       _deviceStorage.getString(storageKey),
@@ -23,11 +19,10 @@ class SelectedRestaurantStorage implements ISelectedRestaurantStorage {
     return RestaurantDto.fromJson(jsonRestaurant).toDomain();
   }
 
-  @override
   Future<bool> setRestaurant(Restaurant restaurant) {
     return _deviceStorage.setString(
       storageKey,
-      RestaurantDto.fromDomain(restaurant).toJson().toString(),
+      jsonEncode(RestaurantDto.fromDomain(restaurant).toJson()),
     );
   }
 }
