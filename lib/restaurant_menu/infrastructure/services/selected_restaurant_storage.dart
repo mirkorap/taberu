684a3ccec8de7c1bed3757e35a6ccbc32ef83af4
Mirko Rapisarda <mirkorap16@gmail.com>
2021-02-14 11:46:01 +0100

    feat: add service to store selected restaurant on device

---
+++ lib/restaurant_menu/infrastructure/services/selected_restaurant_storage.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:injectable/injectable.dart';
+import 'package:taberu/core/application/services/i_device_storage.dart';
+import 'package:taberu/restaurant_menu/application/services/i_selected_restaurant_storage.dart';
+import 'package:taberu/restaurant_menu/domain/entities/restaurant.dart';
+import 'package:taberu/restaurant_menu/infrastructure/data_transfer_objects/restaurant_dto.dart';
+
+@LazySingleton(as: ISelectedRestaurantStorage)
+class SelectedRestaurantStorage implements ISelectedRestaurantStorage {
+  static const storageKey = 'selected_restaurant';
+
+  final IDeviceStorage _deviceStorage;
+
+  SelectedRestaurantStorage(this._deviceStorage);
+
+  @override
+  Restaurant getRestaurant() {
+    final jsonRestaurant = jsonDecode(
+      _deviceStorage.getString(storageKey),
+    ) as Map<String, dynamic>;
+
+    return RestaurantDto.fromJson(jsonRestaurant).toDomain();
+  }
+
+  @override
+  Future<bool> setRestaurant(Restaurant restaurant) {
+    return _deviceStorage.setString(
+      storageKey,
+      RestaurantDto.fromDomain(restaurant).toJson().toString(),
+    );
+  }
+}
