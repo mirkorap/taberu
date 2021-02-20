bf5683afad3f13854dfa3b272aa8413f4de134ed
Mirko Rapisarda <mirkorap16@gmail.com>
2021-02-20 15:21:38 +0100

    fix: change package "cloud_firestore" with "firebase_core"

--- lib/core/infrastructure/extension_methods/firebase_core.dart
+++ lib/core/infrastructure/extension_methods/firebase_core.dart
@@ -1,4 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 extension FirebaseExceptionX on FirebaseException {
   bool get isPermissionDeniedException => code.contains('permission-denied');
