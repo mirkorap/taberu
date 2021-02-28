51cca25150e6bce1e02ea63cebac4e9cae571a69
Mirko Rapisarda <mirkorap16@gmail.com>
2021-02-28 14:44:23 +0100

    feat: add bottom navigation menu widget

---
+++ lib/restaurant_menu/presentation/dishes_selection/widgets/bottom_navigation_menu.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:stilo/stilo.dart';
+import 'package:taberu/restaurant_menu/presentation/dishes_selection/widgets/bottom_navigation_menu_item.dart';
+import 'package:taberu/router.gr.dart';
+
+class BottomNavigationMenu extends StatelessWidget {
+  const BottomNavigationMenu({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      color: StiloColor.transparent,
+      elevation: StiloElevation.z0,
+      child: Container(
+        height: 60.0,
+        decoration: const BoxDecoration(
+          color: StiloColor.white,
+          borderRadius: StiloBorderRadius.t7,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const BottomNavigationMenuItem(
+              routeName: Routes.dishesSelectionScreen,
+              activeIcon: Icons.home,
+              inactiveIcon: Icons.home_outlined,
+            ),
+            const BottomNavigationMenuItem(
+              routeName: null, // TODO: add routeName
+              activeIcon: Icons.shopping_cart,
+              inactiveIcon: Icons.shopping_cart_outlined,
+            ),
+            const BottomNavigationMenuItem(
+              routeName: null, // TODO: add routeName
+              activeIcon: Icons.add_shopping_cart,
+              inactiveIcon: Icons.add_shopping_cart_outlined,
+            ),
+            const BottomNavigationMenuItem(
+              routeName: null, // TODO: add routeName
+              activeIcon: Icons.restaurant_menu,
+              inactiveIcon: Icons.restaurant_menu_outlined,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/restaurant_menu/presentation/dishes_selection/widgets/bottom_navigation_menu_item.dart
@@ -0,0 +1,33 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:stilo/stilo.dart';
+import 'package:taberu/themes/app_color.dart';
+
+class BottomNavigationMenuItem extends StatelessWidget {
+  final String routeName;
+  final IconData activeIcon;
+  final IconData inactiveIcon;
+
+  const BottomNavigationMenuItem({
+    Key key,
+    @required this.routeName,
+    @required this.activeIcon,
+    @required this.inactiveIcon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final isCurrentRoute = ModalRoute.of(context).settings.name == routeName;
+
+    return IconButton(
+      onPressed: () {
+        if (!isCurrentRoute) {
+          ExtendedNavigator.of(context).replace(routeName);
+        }
+      },
+      color: isCurrentRoute ? AppColor.primary : StiloColor.black50,
+      icon: Icon(isCurrentRoute ? activeIcon : inactiveIcon),
+      iconSize: 30.0,
+    );
+  }
+}
