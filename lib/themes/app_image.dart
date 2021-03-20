1fd0fcd706c7797317fc3d3a6ee8a4a29da87332
Mirko Rapisarda <mirkorap16@gmail.com>
2021-03-20 15:26:36 +0100

    chore: add placeholder image for dish

--- lib/themes/app_image.dart
+++ lib/themes/app_image.dart
@@ -1,4 +1,5 @@
 abstract class AppImage {
   static const logo = 'assets/images/logo.png';
-  static const splashScreen = 'assets/images/splash-screen.jpg';
+  static const splashScreen = 'assets/images/restaurant_menu/splash_screen.jpg';
+  static const dishPlaceholder = 'assets/images/restaurant_menu/dish_placeholder.jpg';
 }
