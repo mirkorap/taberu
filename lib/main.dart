e0cacdbb9d36082e52977eb431010934cb3e314b
Mirko Rapisarda <mirkorap16@gmail.com>
2021-03-27 10:05:58 +0100

    refactor: move TaberuApp widget outside of core module

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,30 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
-import 'package:taberu/core/presentation/taberu_app.dart';
 import 'package:taberu/injection.dart';
 import 'package:taberu/locale.dart';
+import 'package:taberu/router.gr.dart' as router;
+import 'package:taberu/themes/app_theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   configureInjection(Environment.prod);
   await Firebase.initializeApp();
   runApp(const AppLocale(child: TaberuApp()));
+}
+
+class TaberuApp extends StatelessWidget {
+  const TaberuApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: tr('app.title'),
+      debugShowCheckedModeBanner: false, // TODO: hide based on environment
+      theme: AppTheme.build(),
+      builder: ExtendedNavigator.builder(router: router.Router()),
+    );
+  }
 }
