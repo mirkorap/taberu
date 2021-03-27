693026058da9bda07c1ee58179d7c4331a9d3774
Mirko Rapisarda <mirkorap16@gmail.com>
2021-03-27 14:58:40 +0100

    refactor: replace bottom navigation bar with curved animation bar

--- lib/restaurant_menu/presentation/widgets/bottom_navigation_menu/bottom_navigation_menu.dart
+++ lib/restaurant_menu/presentation/widgets/bottom_navigation_menu/bottom_navigation_menu.dart
@@ -1,48 +1,37 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:stilo/stilo.dart';
-import 'package:taberu/restaurant_menu/presentation/widgets/bottom_navigation_menu/bottom_navigation_menu_item.dart';
 import 'package:taberu/router.gr.dart';
+import 'package:taberu/themes/app_color.dart';
 
 class BottomNavigationMenu extends StatelessWidget {
+  static const routes = [
+    Routes.dishesSelectionScreen,
+    Routes.dishesSelectionScreen,
+    Routes.dishesSelectionScreen,
+    Routes.dishesSelectionScreen,
+  ];
+
   const BottomNavigationMenu({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BottomAppBar(
-      color: StiloColor.transparent,
-      elevation: StiloElevation.z0,
-      child: Container(
-        height: StiloHeight.h16,
-        decoration: const BoxDecoration(
-          color: StiloColor.white,
-          boxShadow: StiloBoxShadow.lg,
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            const BottomNavigationMenuItem(
-              routeName: Routes.dishesSelectionScreen,
-              activeIcon: Icons.home,
-              inactiveIcon: Icons.home_outlined,
-            ),
-            const BottomNavigationMenuItem(
-              routeName: null, // TODO: add routeName
-              activeIcon: Icons.shopping_cart,
-              inactiveIcon: Icons.shopping_cart_outlined,
-            ),
-            const BottomNavigationMenuItem(
-              routeName: null, // TODO: add routeName
-              activeIcon: Icons.add_shopping_cart,
-              inactiveIcon: Icons.add_shopping_cart_outlined,
-            ),
-            const BottomNavigationMenuItem(
-              routeName: null, // TODO: add routeName
-              activeIcon: Icons.restaurant_menu,
-              inactiveIcon: Icons.restaurant_menu_outlined,
-            ),
-          ],
-        ),
-      ),
+    return CurvedNavigationBar(
+      backgroundColor: AppColor.primary,
+      height: StiloHeight.h16,
+      items: [
+        const Icon(Icons.home_outlined, size: 30.0),
+        const Icon(Icons.shopping_cart_outlined, size: 30.0),
+        const Icon(Icons.add_shopping_cart_outlined, size: 30.0),
+        const Icon(Icons.restaurant_menu_outlined, size: 30.0),
+      ],
+      onTap: (index) {
+        final isCurrentRoute = ModalRoute.of(context).settings.name == routes[index];
+        if (!isCurrentRoute) {
+          ExtendedNavigator.of(context).replace(routes[index]);
+        }
+      },
     );
   }
 }
