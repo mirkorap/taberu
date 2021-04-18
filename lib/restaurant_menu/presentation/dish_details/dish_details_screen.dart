5ab22d4015b0ffde3195864efe35de319cf77141
Mirko Rapisarda <mirkorap16@gmail.com>
2021-04-18 10:25:15 +0200

    feat: implement screen to display dish details

--- lib/restaurant_menu/presentation/dish_details/dish_details_screen.dart
+++ lib/restaurant_menu/presentation/dish_details/dish_details_screen.dart
@@ -1,5 +1,11 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:stilo/stilo.dart';
+import 'package:taberu/core/presentation/widgets/navigation_bars/top_navigation_bar.dart';
 import 'package:taberu/restaurant_menu/domain/entities/dish.dart';
+import 'package:taberu/restaurant_menu/presentation/dish_details/widgets/dish_image_gallery.dart';
+import 'package:taberu/restaurant_menu/presentation/dish_details/widgets/dish_information.dart';
+import 'package:taberu/themes/app_text_style.dart';
 
 class DishDetailsScreen extends StatelessWidget {
   final Dish dish;
@@ -12,8 +18,35 @@ class DishDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Hello World'),
+      appBar: const TopNavigationBar(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Center(
+              child: Column(
+                children: [
+                  DishImageGallery(gallery: dish.gallery.getOrCrash()),
+                  StiloSpacing.y10,
+                  Text(dish.name, style: Theme.of(context).textTheme.headline2),
+                  StiloSpacing.y4,
+                  Text(dish.price.toString(), style: AppTextStyle.priceText),
+                ],
+              ),
+            ),
+            StiloSpacing.y10,
+            DishInformation(dish: dish),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: StiloEdge.edge5,
+          horizontal: StiloEdge.edge10,
+        ),
+        child: TextButton(
+          onPressed: () {},
+          child: const Text('app.actions.add_to_order').tr(),
+        ),
       ),
     );
   }
