b7f6811e44b76e92e4495743fc8b2207899dd56f
Mirko Rapisarda <mirkorap16@gmail.com>
2021-04-25 10:29:03 +0200

    feat: add icons: Facebook and Instagram

---
+++ lib/themes/app_icon.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class AppIcon {
+  static const _fontFamily = 'Taberu';
+
+  static const instagram = IconData(0xf16d, fontFamily: _fontFamily);
+  static const facebook = IconData(0xf39e, fontFamily: _fontFamily);
+}
