7a83fa5fd0ec8ddb08a31ac3bbfe734a883283f4
Mirko Rapisarda <mirkorap16@gmail.com>
2021-06-13 12:02:13 +0200

    feat: change routing generation after auto_route update

--- lib/router.dart
+++ lib/router.dart
@@ -1,16 +1,18 @@
-import 'package:auto_route/auto_route_annotations.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:taberu/core/presentation/splash/splash_screen.dart';
 import 'package:taberu/restaurant_menu/presentation/dish_details/dish_details_screen.dart';
 import 'package:taberu/restaurant_menu/presentation/dishes_selection/dishes_selection_screen.dart';
 import 'package:taberu/restaurant_menu/presentation/restaurant_contacts/restaurant_contacts_screen.dart';
 import 'package:taberu/restaurant_menu/presentation/restaurant_selection/restaurant_selection_screen.dart';
 
 @MaterialAutoRouter(
+  replaceInRouteName: 'Page,Route',
   routes: <AutoRoute>[
-    MaterialRoute(page: RestaurantSelectionScreen),
-    MaterialRoute(page: DishesSelectionScreen),
-    MaterialRoute(page: RestaurantContactsScreen),
-    MaterialRoute(page: DishDetailsScreen, fullscreenDialog: true),
+    AutoRoute(page: SplashScreen),
+    AutoRoute(page: RestaurantSelectionScreen),
+    AutoRoute(page: DishesSelectionScreen),
+    AutoRoute(page: RestaurantContactsScreen),
+    AutoRoute(page: DishDetailsScreen, fullscreenDialog: true),
   ],
-  generateNavigationHelperExtension: true,
 )
-class $Router {}
+class $AppRouter {}
