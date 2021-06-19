a7b2e1b7e2e26c668d535cd283e91e715aae9ee2
Mirko Rapisarda <mirkorap16@gmail.com>
2021-06-19 11:26:02 +0200

    feat: add params to perform actions before navigate to next route

--- lib/core/presentation/splash/splash_screen.dart
+++ lib/core/presentation/splash/splash_screen.dart
@@ -1,31 +1,46 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:stilo/stilo.dart';
 import 'package:taberu/themes/app_color.dart';
 import 'package:taberu/themes/app_image.dart';
 
 class SplashScreen extends StatelessWidget {
-  const SplashScreen({Key? key}) : super(key: key);
+  final PageRouteInfo nextRoute;
+  final Future beforeNavigation;
+
+  const SplashScreen({
+    Key? key,
+    required this.nextRoute,
+    required this.beforeNavigation,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Stack(
-        children: [
-          Image.asset(
-            AppImage.splashScreen,
-            fit: BoxFit.cover,
-            width: double.infinity,
-            height: double.infinity,
-          ),
-          Container(
-            alignment: Alignment.center,
-            margin: StiloEdge.top36,
-            child: const CircularProgressIndicator(
-              valueColor: AppColor.circularProgressIndicator,
-            ),
-          ),
-        ],
+    return FutureBuilder(
+      future: beforeNavigation.whenComplete(
+        () => context.replaceRoute(nextRoute),
       ),
+      builder: (context, snapshot) {
+        return SafeArea(
+          child: Stack(
+            children: [
+              Image.asset(
+                AppImage.splashScreen,
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: double.infinity,
+              ),
+              Container(
+                alignment: Alignment.center,
+                margin: StiloEdge.top36,
+                child: const CircularProgressIndicator(
+                  valueColor: AppColor.circularProgressIndicator,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
