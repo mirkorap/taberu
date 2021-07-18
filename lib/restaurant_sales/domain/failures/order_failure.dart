dbc495ec072340134b2da3dfcf9bc5f29ff9da9d
Mirko Rapisarda <mirkorap16@gmail.com>
2021-07-18 15:47:03 +0200

    feat: add class to manage order failures

---
+++ lib/restaurant_sales/domain/failures/order_failure.dart
@@ -0,0 +1,12 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'order_failure.freezed.dart';
+
+@freezed
+class OrderFailure with _$OrderFailure {
+  const factory OrderFailure.insufficientPermissions() = _InsufficientPermissions;
+
+  const factory OrderFailure.unableToUpdate() = _UnableToUpdate;
+
+  const factory OrderFailure.unexpected() = _Unexpected;
+}
