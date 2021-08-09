730797ec5099cd5dec21b580a6eef1beb1582fc2
Mirko Rapisarda <mirkorap16@gmail.com>
2021-08-09 16:10:34 +0200

    feat: add screen to show message box to user

---
+++ lib/core/presentation/message_box/message_box_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:stilo/stilo.dart';
+import 'package:taberu/core/presentation/widgets/navigation_bars/top_navigation_bar.dart';
+import 'package:taberu/themes/app_color.dart';
+
+class MessageBoxScreen extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final String subtitle;
+
+  const MessageBoxScreen({
+    Key? key,
+    required this.icon,
+    required this.title,
+    required this.subtitle,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const TopNavigationBar(),
+      body: Padding(
+        padding: StiloEdge.all10,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                icon,
+                color: AppColor.secondary,
+                size: 115.0,
+              ),
+              StiloSpacing.vert2,
+              Text(
+                title,
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headline2,
+              ),
+              StiloSpacing.vert2,
+              Text(
+                subtitle,
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/router.dart
+++ lib/router.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:taberu/core/presentation/message_box/message_box_screen.dart';
 import 'package:taberu/core/presentation/splash/splash_screen.dart';
 import 'package:taberu/restaurant_menu/presentation/dish_details/dish_details_screen.dart';
 import 'package:taberu/restaurant_menu/presentation/dishes_selection/dishes_selection_screen.dart';
@@ -9,6 +10,7 @@ import 'package:taberu/restaurant_menu/presentation/restaurant_selection/restaur
   replaceInRouteName: 'Page,Route',
   routes: <AutoRoute>[
     AutoRoute(page: SplashScreen),
+    AutoRoute(page: MessageBoxScreen, fullscreenDialog: true),
     AutoRoute(page: RestaurantSelectionScreen),
     AutoRoute(page: DishesSelectionScreen),
     AutoRoute(page: RestaurantContactsScreen),
