cf551c499ca62571a457ca3723dc16a07427626f
Mirko Rapisarda <mirkorap16@gmail.com>
2021-08-13 11:48:46 +0200

    feat: add screen widget to display empty cart message box

---
+++ lib/restaurant_sales/presentation/empty_cart/empty_cart_screen.dart
@@ -0,0 +1,20 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:taberu/core/presentation/message_box/message_box_screen.dart';
+import 'package:taberu/router.gr.dart';
+
+class EmptyCartScreen extends StatelessWidget {
+  const EmptyCartScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MessageBoxScreen(
+      onPressed: () => context.replaceRoute(const DishesSelectionScreen()),
+      buttonText: tr('empty_cart.button_text'),
+      icon: Icons.shopping_cart_outlined,
+      title: tr('empty_cart.title'),
+      subtitle: tr('empty_cart.subtitle'),
+    );
+  }
+}
