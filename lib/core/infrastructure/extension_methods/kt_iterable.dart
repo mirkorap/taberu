84376abcbfc421a67a3f4d3d4d3196da7b91c8de
Mirko Rapisarda <mirkorap16@gmail.com>
2021-08-14 14:38:53 +0200

    feat: add method to replace old element with a new one

--- lib/core/infrastructure/extension_methods/kt_iterable.dart
+++ lib/core/infrastructure/extension_methods/kt_iterable.dart
@@ -1,6 +1,12 @@
 import 'package:kt_dart/collection.dart';
 
 extension KtIterableX<T> on KtIterable<T> {
+  KtList<T> replace(T oldElement, T newElement) {
+    final index = indexOf(oldElement);
+
+    return replaceAt(index, newElement);
+  }
+
   KtList<T> replaceAt(int index, T element) {
     final list = mutableListFrom(iter);
     list[index] = element;
