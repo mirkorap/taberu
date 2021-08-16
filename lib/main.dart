7a028db5adfce5cd5aa7ba81ddfff757b5c3ae21
Mirko Rapisarda <mirkorap16@gmail.com>
2021-08-16 14:34:06 +0200

    refactor: move logic about global bloc classes inside specific widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,13 @@ import 'package:auto_route/auto_route.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
 import 'package:stilo/stilo.dart';
+import 'package:taberu/bloc.dart';
 import 'package:taberu/injection.dart';
 import 'package:taberu/locale.dart';
-import 'package:taberu/restaurant_menu/application/dish_details/dish_details_cubit.dart';
-import 'package:taberu/restaurant_menu/application/restaurant_selection/restaurant_selection_cubit.dart';
 import 'package:taberu/restaurant_menu/application/services/i_selected_restaurant_storage.dart';
-import 'package:taberu/restaurant_sales/application/cart/cart_cubit.dart';
 import 'package:taberu/restaurant_sales/application/guards/cart_guard.dart';
-import 'package:taberu/restaurant_sales/application/services/i_current_order_storage.dart';
 import 'package:taberu/router.gr.dart';
 import 'package:taberu/themes/app_image.dart';
 import 'package:taberu/themes/app_theme.dart';
@@ -32,55 +28,18 @@ class TaberuApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<RestaurantSelectionCubit>(
-          create: (context) => getIt<RestaurantSelectionCubit>(),
-        ),
-        BlocProvider<DishDetailsCubit>(
-          create: (context) => getIt<DishDetailsCubit>(),
-        ),
-        BlocProvider<CartCubit>(
-          create: (context) => getIt<CartCubit>(),
-        ),
-      ],
-      child: MultiBlocListener(
-        listeners: [
-          BlocListener<RestaurantSelectionCubit, RestaurantSelectionState>(
-            listener: (context, state) {
-              state.maybeWhen(
-                restaurantSelected: (restaurant) {
-                  final currentOrderStorage = getIt<ICurrentOrderStorage>();
-                  currentOrderStorage.clear();
-                },
-                orElse: () => null,
-              );
-            },
-          ),
-          BlocListener<DishDetailsCubit, DishDetailsState>(
-            listener: (context, state) {
-              state.maybeWhen(
-                dishToCartButtonPressed: (dish) {
-                  final cubit = context.read<CartCubit>();
-                  cubit.addOneToCart(dish);
-                },
-                orElse: () => null,
-              );
-            },
-          ),
-        ],
-        child: MaterialApp.router(
-          title: tr('app.title'),
-          debugShowCheckedModeBanner: false, // TODO: hide based on environment
-          theme: AppTheme.build(),
-          routerDelegate: _appRouter.delegate(initialRoutes: [
-            _buildInitialRoute(context),
-          ]),
-          routeInformationParser: _appRouter.defaultRouteParser(),
-          localizationsDelegates: context.localizationDelegates,
-          supportedLocales: context.supportedLocales,
-          locale: context.locale,
-        ),
+    return AppBloc(
+      child: MaterialApp.router(
+        title: tr('app.title'),
+        debugShowCheckedModeBanner: false, // TODO: hide based on environment
+        theme: AppTheme.build(),
+        routerDelegate: _appRouter.delegate(initialRoutes: [
+          _buildInitialRoute(context),
+        ]),
+        routeInformationParser: _appRouter.defaultRouteParser(),
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        locale: context.locale,
       ),
     );
   }
