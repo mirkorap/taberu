cc821fe742d7b5ff6293d5379ed277cd690b57d3
Mirko Rapisarda <mirkorap16@gmail.com>
2021-08-19 15:11:46 +0200

    feat: add widget to display restaurant table select box

--- lib/restaurant_sales/presentation/checkout/checkout_screen.dart
+++ lib/restaurant_sales/presentation/checkout/checkout_screen.dart
@@ -1,23 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stilo/stilo.dart';
 import 'package:taberu/core/presentation/widgets/navigation_bars/top_navigation_bar.dart';
+import 'package:taberu/injection.dart';
+import 'package:taberu/restaurant_menu/application/restaurant_configuration/restaurant_configuration_cubit.dart';
+import 'package:taberu/restaurant_sales/application/cart/cart_cubit.dart';
 import 'package:taberu/restaurant_sales/presentation/checkout/widgets/order_type_list.dart';
+import 'package:taberu/restaurant_sales/presentation/checkout/widgets/restaurant_table_select_box.dart';
 
 class CheckoutScreen extends StatelessWidget {
   const CheckoutScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const TopNavigationBar(),
-      body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        child: Padding(
-          padding: StiloEdge.horiz8,
-          child: Column(
-            children: [
-              const OrderTypeList(),
-            ],
+    return BlocProvider<RestaurantConfigurationCubit>(
+      create: (context) => getIt<RestaurantConfigurationCubit>()..load(),
+      child: Scaffold(
+        appBar: const TopNavigationBar(),
+        body: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Padding(
+            padding: StiloEdge.horiz8,
+            child: BlocBuilder<CartCubit, CartState>(
+              builder: (context, state) {
+                return Column(
+                  children: [
+                    const OrderTypeList(),
+                    StiloSpacing.vert6,
+                    Visibility(
+                      visible: state.order.isDeliveredAtTable,
+                      child: const RestaurantTableSelectBox(),
+                    ),
+                  ],
+                );
+              },
+            ),
           ),
         ),
       ),
---
+++ lib/restaurant_sales/presentation/checkout/widgets/restaurant_table_select_box.dart
@@ -0,0 +1,43 @@
+import 'package:dropdown_search/dropdown_search.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:stilo/stilo.dart';
+import 'package:taberu/restaurant_menu/application/restaurant_configuration/restaurant_configuration_cubit.dart';
+import 'package:taberu/restaurant_menu/domain/entities/restaurant_table.dart';
+import 'package:taberu/restaurant_menu/infrastructure/extension_methods/kt_configuration.dart';
+import 'package:taberu/themes/app_input.dart';
+
+class RestaurantTableSelectBox extends StatelessWidget {
+  const RestaurantTableSelectBox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<RestaurantConfigurationCubit, RestaurantConfigurationState>(
+      builder: (context, state) {
+        return state.maybeWhen(
+          loadSuccess: (configurations) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'checkout.restaurant_table',
+                  style: Theme.of(context).textTheme.headline2,
+                ).tr(),
+                StiloSpacing.vert4,
+                DropdownSearch<RestaurantTable>(
+                  mode: Mode.MENU,
+                  dropdownSearchDecoration: AppInput.selectBoxField,
+                  items: configurations.restaurantTables.asList(),
+                  itemAsString: (restaurantTable) => restaurantTable.name,
+                  onChanged: (value) {},
+                ),
+              ],
+            );
+          },
+          orElse: () => Container(),
+        );
+      },
+    );
+  }
+}
