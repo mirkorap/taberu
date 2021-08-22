a43129c293f4b32ffa60888b14942d460bc001bf
Mirko Rapisarda <mirkorap16@gmail.com>
2021-08-22 10:10:27 +0200

    feat: update order notes on change event

--- lib/restaurant_sales/presentation/checkout/widgets/order_notes_form.dart
+++ lib/restaurant_sales/presentation/checkout/widgets/order_notes_form.dart
@@ -1,6 +1,8 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stilo/stilo.dart';
+import 'package:taberu/restaurant_sales/application/cart/cart_cubit.dart';
 import 'package:taberu/themes/app_input.dart';
 
 class OrderNotesForm extends StatelessWidget {
@@ -18,6 +20,10 @@ class OrderNotesForm extends StatelessWidget {
           ).tr(),
           StiloSpacing.vert4,
           TextField(
+            onChanged: (value) {
+              final cubit = context.read<CartCubit>();
+              cubit.updateNotes(value);
+            },
             maxLines: 8,
             decoration: AppInput.inputTextField.copyWith(
               hintText: tr('checkout.notes_hint_text'),
