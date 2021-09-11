3976b55b8066db5415c741f7d38bd95688ecba18
Mirko Rapisarda <mirkorap16@gmail.com>
2021-09-11 11:12:11 +0200

    refactor: remove unused order failure

--- lib/restaurant_sales/domain/failures/order_failure.dart
+++ lib/restaurant_sales/domain/failures/order_failure.dart
@@ -6,8 +6,6 @@ part 'order_failure.freezed.dart';
 class OrderFailure with _$OrderFailure {
   const factory OrderFailure.insufficientPermissions() = _InsufficientPermissions;
 
-  const factory OrderFailure.unableToUpdate() = _UnableToUpdate;
-
   const factory OrderFailure.noOrderFound() = _NoOrderFound;
 
   const factory OrderFailure.unexpected() = _Unexpected;
